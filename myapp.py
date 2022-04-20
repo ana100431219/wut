e01ee63b4db8141ba5b0900c0667481690b8597c
Ayalh <99047637+Ayalh@users.noreply.github.com>
2022-04-20 21:09:50 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,4 +5,4 @@ from PIL import Image
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 
-image= Image.open'https://www.google.com/url?sa=i&url=https%3A%2F%2Faeneas-office.org%2Ffunding%2Fkdt%2F&psig=AOvVaw1nlvJOoTt7eBcN5sF4VfeS&ust=1647896445948000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKj3jOTK1fYCFQAAAAAdAAAAABAI'
+image= Image.open('https://www.google.com/url?sa=i&url=https%3A%2F%2Faeneas-office.org%2Ffunding%2Fkdt%2F&psig=AOvVaw1nlvJOoTt7eBcN5sF4VfeS&ust=1647896445948000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKj3jOTK1fYCFQAAAAAdAAAAABAI')
