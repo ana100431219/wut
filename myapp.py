4d5369304858f4c6077117d19ec518563cb2640d
Ayalh <99047637+Ayalh@users.noreply.github.com>
2022-04-20 21:09:17 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
+from PIL import Image
+
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+
+image= Image.open'https://www.google.com/url?sa=i&url=https%3A%2F%2Faeneas-office.org%2Ffunding%2Fkdt%2F&psig=AOvVaw1nlvJOoTt7eBcN5sF4VfeS&ust=1647896445948000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKj3jOTK1fYCFQAAAAAdAAAAABAI'
