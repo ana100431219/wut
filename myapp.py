b90c28b9b5b273e3dc7699533021b55a4a7e0b02
Ayalh <99047637+Ayalh@users.noreply.github.com>
2022-05-06 20:04:01 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,8 +1,19 @@
-import pandas as pd
 import numpy as np
 import streamlit as st
+
+import sqlite3
+import pandas as pd 
+from sqlite3.dbapi2 import DatabaseError
 from PIL import Image
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+con= sqlite3.connect('ecsel_database.db')
+df_participants=pd.read_excel(r'C:\Users\Ayala\Downloads\participants')
+df_countries=pd.read_excel(r'C:\Users\Ayala\Downloads\countries.xlsx')
+df_projects=pd.read_excel(r'C:\Users\Ayala\Downloads\projects.xlsx')
 
-image= Image.open('
+df_projects.to_sql('projects', con, if_exists='replace', index= False)
+df_countries.to_sql('countries', con, if_exists='replace', index= False)
+df_participants.to_sql('participants', con, if_exists='replace', index= False)
+
+con.close()
+df_projects.head()
