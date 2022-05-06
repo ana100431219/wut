109462dcd51a2dc33ee615e1e89543cb854a8e62
Ayalh <99047637+Ayalh@users.noreply.github.com>
2022-05-06 20:58:37 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -9,6 +9,10 @@ df_participants=pd.read_excel('participants.xlsx')
 df_countries=pd.read_excel('countries.xlsx')
 df_projects=pd.read_excel('projects.xlsx')
 
+df_projects.to_sql('projects', con, if_exists='replace', index= False)
+df_countries.to_sql('countries', con, if_exists='replace', index= False)
+df_participants.to_sql('participants', con, if_exists='replace', index= False)
+
 con.close()
 df_projects.head()
 
