54f3d265adde64df73e897f863390f7c4eb3c071
Ayalh <99047637+Ayalh@users.noreply.github.com>
2022-05-11 17:04:16 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,36 +1,12 @@
-database='ecsel_database'
+import sqlite3 
+import pandas as pd 
+import streamlit as st
+from sqlite3.dbapi2 import DatabaseError
+from PIL import Image
 
-acronyms=list(df_countries.Acronym) 
-countries=list(df_countries.Country)
-answer= input("Please, input the name of an acronym or a country: ")
-while answer.upper() not in acronyms and answer.title() not in countries : 
-    answer= input("Please, input the name of an acronym or a country ")
-if answer.upper() in acronyms :
-  answer_acronym = answer.upper()
-  answer_name = df_countries[df_countries.Acronym==answer_acronym]
-elif answer.title() in countries:
-  answer_name = answer.title()
-  answer_acronym = df_countries[df_countries.Country== answer_name].Acronym.item()
+#colnames={c:c for c in list(df_countries)}
 
-print('Selected:{}-{}'.format(answer_acronym,answer_name))    
-
-df_selcountry=df_participants[df_participants.country==answer_acronym]
-df_projyear=df_projects[["projectID","year"]]
-df_selcountry=pd.merge(df_selcountry,df_projyear,how="left",on="projectID")
-
-df_ECcontr_year=df_selcountry[["ecContribution","year"]].groupby(by="year").sum()
-df_ECcontr_year=df_selcountry.groupby('year').sum().ecContribution
-df_ECcontr_year.plot(kind='bar', title='Total EU contribution in {ct} (M€)')
-
-df_ECcontr_year.describe()
-
-df_best=df_selcountry.groupby(['shortName','name','activityType','organizationURL']).agg({'ecContribution':['count', 'sum']}).sort_values([("ecContribution","sum")],ascending=False)
-df_best.to_excel("country_participants.xlsx")
-df_best.head()
-
-
-#colnames={c:c for c in list(df)}
-database = con
+database = 'CreateDB.db'
 selects= {
 'country':
 '''SELECT Acronym FROM countries WHERE Country = "{}" ''',
