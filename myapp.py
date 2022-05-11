997d9c3c8475383ebc3884486e23b8a162cbac13
Ayalh <99047637+Ayalh@users.noreply.github.com>
2022-05-11 20:06:08 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -56,7 +56,7 @@ df_grants_year = pd.read_sql('''SELECT j.year, SUM(p.ecContribution) AS grants
 
 #grants
 st.subheader(f'Yearly EC contribution in {ct} (€)')
-st.bar_chart(df_grants_year)
+st.bar_chart(dfs[grants])
 
 #participants
 st.subheader(f'Participants in {ct}')
